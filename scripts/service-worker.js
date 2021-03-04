09696c4cc7c448098c4e14896f6c618a2c646730
shoredevin <36136055+shoredevin@users.noreply.github.com>
2021-03-04 15:47:10 -0500

    Update service-worker.js

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,8 +1,35 @@
-// Listen for install event, set callback
-self.addEventListener('install', function(event) {
-    // Perform some task
+ar cacheName = 'v1:static';
+
+// During the installation phase, you'll usually want to cache static assets.
+self.addEventListener('install', function(e) {
+    // Once the service worker is installed, go ahead and fetch the resources to make this work offline.
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            return cache.addAll([
+                './',
+                './css/style.css',
+                './js/build/script.min.js',
+                './js/build/vendor.min.js',
+                './css/fonts/roboto.woff',
+                './offline.html'
+            ]).then(function() {
+                self.skipWaiting();
+            });
+        })
+    );
 });
 
-self.addEventListener('activate', function(event) {
-  // Perform some task
+// when the browser fetches a URL…
+self.addEventListener('fetch', function(event) {
+    // … either respond with the cached object or go ahead and fetch the actual URL
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                // retrieve from cache
+                return response;
+            }
+            // fetch as normal
+            return fetch(event.request);
+        })
+    );
 });
