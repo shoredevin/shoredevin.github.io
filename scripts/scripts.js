fc45695f3f07533a5fd39862460f2ec2bed434a9
shoredevin <36136055+shoredevin@users.noreply.github.com>
2021-03-04 16:06:38 -0500

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,36 @@
 let totalRows;
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/scripts/service-worker.js')
-  .then(function(registration) {
-    console.log('Registration successful, scope is:', registration.scope);
-  })
-  .catch(function(error) {
-    console.log('Service worker registration failed, error:', error);
-  });
+    navigator.serviceWorker.register('./sw.js').then(function(reg) {
+        console.log('Successfully registered service worker', reg);
+    }).catch(function(err) {
+        console.warn('Error whilst registering service worker', err);
+    });
 }
+
+window.addEventListener('online', function(e) {
+    // Resync data with server.
+    console.log("You are online");
+    Page.hideOfflineWarning();
+    Arrivals.loadData();
+}, false);
+
+window.addEventListener('offline', function(e) {
+    // Queue up events for server.
+    console.log("You are offline");
+    Page.showOfflineWarning();
+}, false);
+
+// Check if the user is connected.
+if (navigator.onLine) {
+    Arrivals.loadData();
+} else {
+    // Show offline message
+    Page.showOfflineWarning();
+}
+
+// Set Knockout view model bindings.
+ko.applyBindings(Page.vm);
 
 //search logic
 $(document).ready(function () {
