aa35c511df18433bbcacc645196f5f8b940add71
shoredevin <36136055+shoredevin@users.noreply.github.com>
2021-03-04 14:41:56 -0500

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,22 @@
 let totalRows;
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
+
+    if(reg.installing) {
+      console.log('Service worker installing');
+    } else if(reg.waiting) {
+      console.log('Service worker installed');
+    } else if(reg.active) {
+      console.log('Service worker active');
+    }
+
+  }).catch(function(error) {
+    // registration failed
+    console.log('Registration failed with ' + error);
+  });
+}
+
 //search logic
 $(document).ready(function () {
     // console.log(tableRows);
